29a7a8acdef423ffa320635ac117fc275f0c4e11
vcatic <vanesa.cat09@gmail.com>
2024-11-05 19:32:40 +0100

    added pointCounter to game html

--- quarkus/src/main/resources/META-INF/resources/spiel.js
+++ quarkus/src/main/resources/META-INF/resources/spiel.js
@@ -1,11 +1,14 @@
 
 let team1 = document.getElementById("team1").save
 let team2 =document.getElementById("team2").save
-
+let counter1 =0
+let counter2 =0
+let pointCounter = 0
 
 function addPoint(teamNum){
     let teamName = document.getElementById(`team${teamNum}`);
-    let counter ="|"
+
+
     fetch(`http://localhost:8080/game/modifyScore?team=team${teamNum}&type=+`, {
         method: 'post',
         body: teamName,
@@ -17,8 +20,17 @@ function addPoint(teamNum){
         }
     })
 
-    document.getElementById(`counter${teamNum}`).innerText += counter
+    counter[teamNum]++
+    if (counter === 9){
+        pointCounter++
+        counter =""
+        document.getElementById(`pointCounter${teamNum}`).innerText = pointCounter
+    }
+    document.getElementById(`counter${teamNum}`).innerText = counter[teamNum]
+
 }
+
+
 
 function deletePoint(teamNum){
     let teamName = document.getElementById(`team${teamNum}`);
@@ -39,4 +51,6 @@ function deletePoint(teamNum){
     if (counterText.length > 0) {
         counterElement.innerText = counterText.slice(0, -1);
     }
+
+    document.getElementById("teamName1").innerText = team2
 }
