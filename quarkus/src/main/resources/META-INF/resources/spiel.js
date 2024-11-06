37ebb990cb8f4fa497e58eb934d31b854bc382cd
vcatic <vanesa.cat09@gmail.com>
2024-11-06 08:43:21 +0100

    changed point counter in game js

--- quarkus/src/main/resources/META-INF/resources/spiel.js
+++ quarkus/src/main/resources/META-INF/resources/spiel.js
@@ -3,12 +3,13 @@ let team1 = document.getElementById("team1").save
 let team2 =document.getElementById("team2").save
 
 
+
 const counter = {}; // to store each team's 'counter' value
 const pointCounter = {}; //
 
-function addPoint(teamNum){
+function addPoint(teamNum) {
     let teamName = document.getElementById(`team${teamNum}`);
-
+    let counterBox = document.getElementById(`counter${teamNum}`)
     if (!(teamNum in counter)) {
         counter[teamNum] = 0;
         pointCounter[teamNum] = 0;
@@ -17,21 +18,26 @@ function addPoint(teamNum){
     fetch(`http://localhost:8080/game/modifyScore?team=team${teamNum}&type=+`, {
         method: 'post',
         body: teamName,
-    }).then((response) =>{
-        if(!response.ok){
+    }).then((response) => {
+        if (!response.ok) {
             console.log(response.status)
-        }else{
+        } else {
             console.log("Point added Successfully")
         }
     })
 
+
+
     counter[teamNum]++;
-    if (counter[teamNum] === 9){
+    if (counter[teamNum] === 1) {
+        counterBox.innerText += "3"
+    }else if (counter[teamNum] === 2) {
+       counterBox.innerText += "6"
+    }else if (counter[teamNum] === 3){
+        counterBox.innerText += "9"
         pointCounter[teamNum]++
-        counter[teamNum] =""
         document.getElementById(`pointCounter${teamNum}`).innerText = pointCounter[teamNum]
     }
-    document.getElementById(`counter${teamNum}`).innerText = counter[teamNum]
 
     if (pointCounter[teamNum] === 5){
 
@@ -59,6 +65,7 @@ function deletePoint(teamNum){
     let counterText = counterElement.innerText;
     if (counterText.length > 0) {
         counterElement.innerText = counterText.slice(0, -1);
+        counter[teamNum]--
     }
 
     document.getElementById("teamName1").innerText = team2
