6dc64924f965efa9b5e5f781f9579f074264ee74
FlorianHayer <f.hayer@students.htl-leonding.ac.at>
2024-11-19 08:14:24 +0100

    feat: added Turn class

---
+++ quarkus/src/main/java/at/ac/htlleonding/entities/Turn.java
@@ -0,0 +1,18 @@
+package at.ac.htlleonding.entities;
+
+import jakarta.persistence.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Entity
+public class Turn {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long turnId;
+    @OneToMany
+    @JoinColumn
+    private final List<Score> scores = new LinkedList<>();
+
+}
