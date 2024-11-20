ad2a5158a6f93176431b28bdb7f20cd8c6c3b720
FlorianHayer <f.hayer@students.htl-leonding.ac.at>
2024-11-20 08:08:48 +0100

    feat: added TurnRepository

---
+++ quarkus/src/main/java/at/ac/htlleonding/repositories/TurnRepository.java
@@ -0,0 +1,9 @@
+package at.ac.htlleonding.repositories;
+
+import at.ac.htlleonding.entities.Turn;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class TurnRepository implements PanacheRepository<Turn> {
+}
