226cbd7715593ad9a8d452c90235eeaf6eef377e
FlorianHayer <f.hayer@students.htl-leonding.ac.at>
2025-01-14 08:36:16 +0100

    feat: implemented methods in GameRepository

--- quarkus/src/main/java/at/ac/htlleonding/repositories/GameRepository.java
+++ quarkus/src/main/java/at/ac/htlleonding/repositories/GameRepository.java
@@ -1,11 +1,27 @@
 package at.ac.htlleonding.repositories;
 
 import at.ac.htlleonding.entities.Game;
+import at.ac.htlleonding.entities.Stage;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Singleton;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class GameRepository implements PanacheRepository<Game> {
+    @Inject
+    TeamRepository teamRepository;
 
+
+    public Game findById(long id) {
+        return find("id", id).firstResult();
+    }
+
+    public Game createGameWithTeams(long team1Id, long team2Id) {
+        Game game = new Game();
+
+        game.teams.add(teamRepository.findById(team1Id));
+        game.teams.add(teamRepository.findById(team2Id));
+
+        return game;
+    }
 }
