9be7c6c45c7d0eab47871207612a88ae74a9ce19
marvin-anderson <m.anderson@students.htl-leonding.ac.at>
2025-01-29 00:11:19 +0100

    Fix: minor changes on Game, preparing for model

--- quarkus/src/main/resources/META-INF/resources/src/game.ts
+++ quarkus/src/main/resources/META-INF/resources/src/game.ts
@@ -1,4 +1,5 @@
 
+
 let t1SubScore = 0;
 let t1SuperScore = 0;
 let t2SubScore = 0;
@@ -12,26 +13,14 @@ async function loadGame(){
         sessionStorage.setItem('currentGameId', gameId);
         console.log(sessionStorage.getItem('currentGameId'))
 
-        // get info (debugging)
-        let response2 = await fetch(`http://localhost:8080/api/games/getGameInfo?gameId=${sessionStorage.getItem("currentGameId")}`);
-        let gameInfo = await response2.text();
-        console.log(gameInfo);
-
-        // creates a turn using stageId
-        let responseStage = await fetch(`http://localhost:8080/api/games/getCurrentStage?gameId=${sessionStorage.getItem("currentGameId")}`)
-        let stageId = await responseStage.text()
-        let response3 = await fetch(`http://localhost:8080/api/turns/newTurn?stageId=${sessionStorage.getItem(stageId)}`);
-        let turnData = await response3.text();
-        sessionStorage.setItem('currentTurn', turnData);
-        console.log(turnData)
-
     } catch (error) {
         console.error("Error:", error);
     }
 }
 
-function addPoint(teamId: number) {
-    if(teamId == 1){
+
+function addPoint(index: number) {
+    if(index == 1){
         t1SubScore++;
         updateText()
         if(t1SubScore > 2){
