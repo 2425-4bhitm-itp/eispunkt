3076af5d068b76957bd35e27f2e414c0c3e3d5f9
marvin-anderson <m.anderson@students.htl-leonding.ac.at>
2025-01-29 00:12:05 +0100

    Feat: create Stage Route added

--- quarkus/src/main/java/at/ac/htlleonding/routes/StageResource.java
+++ quarkus/src/main/java/at/ac/htlleonding/routes/StageResource.java
@@ -1,15 +1,23 @@
 package at.ac.htlleonding.routes;
 
+import at.ac.htlleonding.entities.Game;
+import at.ac.htlleonding.entities.Stage;
+import at.ac.htlleonding.repositories.GameRepository;
 import at.ac.htlleonding.repositories.StageRepository;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Response;
 
 @Path("api/stages")
 public class StageResource {
     @Inject
     StageRepository stageRepository;
+
+    @Inject
+    GameRepository gameRepository;
 
     @GET
     public Response getStages() {
@@ -19,5 +27,15 @@ public class StageResource {
     @GET
     public Response getStageById(long id) {
         return Response.ok(stageRepository.findById(id)).build();
+    }
+
+    @POST
+    public Response createStage(@QueryParam("gameId") long gameId) {
+        if(gameRepository.findById(gameId) == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }else{
+            gameRepository.findById(gameId).stages.add(new Stage());
+            return Response.ok().build();
+        }
     }
 }
