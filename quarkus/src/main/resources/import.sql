9525cb5a9fcbfdeb4ad83995080670cf77c02ee1
FlorianHayer <f.hayer@students.htl-leonding.ac.at>
2025-02-10 09:10:19 +0100

    feat: added more Testdata

--- quarkus/src/main/resources/import.sql
+++ quarkus/src/main/resources/import.sql
@@ -1,12 +1,54 @@
-insert into team (name) values ('Schueler');
-insert into team (name) values ('Lehrer');
+insert into team (name) values ('ÖTSU Oepping 1');
+insert into team (name) values ('ATSV Waldneukirchen Stocksport 1');
+insert into team (name) values ('ÖTSU Arnreit 1');
+insert into team (name) values ('Union Stocksport Putzleinsdorf 1');
+insert into team (name) values ('ESV Ruprechtshofen 1');
+insert into team (name) values ('ÖTSU Strolheim 1');
+insert into team (name) values ('Union ESV Haag a. H. 1');
+insert into team (name) values ('ÖTSU Oepping 2');
+insert into team (name) values ('SU Schardenberg 2');
 
-insert into player (players_teamid, name) values(1, 'Marvin');
-insert into player (players_teamid, name) values(1, 'Vanesa');
-insert into player (players_teamid, name) values(1, 'Atilla');
-insert into player (players_teamid, name) values(1, 'Florian');
+insert into player (team_teamid, name) values(1, 'Christian Spindler');
+insert into player (team_teamid, name) values(1, 'Johann Märzinger');
+insert into player (team_teamid, name) values(1, '');
+insert into player (team_teamid, name) values(1, '');
 
-insert into player (players_teamid, name) values(2, 'Prof. Stuetz');
-insert into player (players_teamid, name) values(2, 'Prof. Aberger');
-insert into player (players_teamid, name) values(2, 'Prof. Hammer');
-insert into player (players_teamid, name) values(2, 'Prof. Steiner');
+insert into player (team_teamid, name) values(2, 'Johann Wallner');
+insert into player (team_teamid, name) values(2, 'Robert Obengruber');
+insert into player (team_teamid, name) values(2, '');
+insert into player (team_teamid, name) values(2, '');
+
+insert into player (team_teamid, name) values(3, 'Peter Sommer');
+insert into player (team_teamid, name) values(3, 'Kurt Neundinger');
+insert into player (team_teamid, name) values(3, '');
+insert into player (team_teamid, name) values(3, '');
+
+insert into player (team_teamid, name) values(4, 'Robert Höglinger');
+insert into player (team_teamid, name) values(4, 'Gottfried Peer');
+insert into player (team_teamid, name) values(4, '');
+insert into player (team_teamid, name) values(4, '');
+
+insert into player (team_teamid, name) values(5, 'Helmut Stadlbauer');
+insert into player (team_teamid, name) values(5, 'Karl Lettner');
+insert into player (team_teamid, name) values(5, '');
+insert into player (team_teamid, name) values(5, '');
+
+insert into player (team_teamid, name) values(6, 'Günter Reisinger');
+insert into player (team_teamid, name) values(6, 'Friedrich Mairhuber');
+insert into player (team_teamid, name) values(6, '');
+insert into player (team_teamid, name) values(6, '');
+
+insert into player (team_teamid, name) values(7, 'Andreas Gervasi');
+insert into player (team_teamid, name) values(7, 'Rupert Taxler');
+insert into player (team_teamid, name) values(7, '');
+insert into player (team_teamid, name) values(7, '');
+
+insert into player (team_teamid, name) values(8, 'Peter Magauer');
+insert into player (team_teamid, name) values(8, 'Reinhold Stallinger');
+insert into player (team_teamid, name) values(8, '');
+insert into player (team_teamid, name) values(8, '');
+
+insert into player (team_teamid, name) values(9, 'Johann Duscher');
+insert into player (team_teamid, name) values(9, 'Josef Bauer');
+insert into player (team_teamid, name) values(9, '');
+insert into player (team_teamid, name) values(9, '');
