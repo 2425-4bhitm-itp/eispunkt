30097924bb7e71086c033c9b21b89d7476265869
FlorianHayer <f.hayer@students.htl-leonding.ac.at>
2025-02-11 08:18:25 +0100

    feat: added and improved more Testdata

--- quarkus/src/main/resources/import.sql
+++ quarkus/src/main/resources/import.sql
@@ -1,54 +1,108 @@
 insert into team (name) values ('ÖTSU Oepping 1');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Oepping 1'), 'Christian Spindler (AT402740)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Oepping 1'), 'Johann Märzinger (AT402728)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Oepping 1'), 'Franz Steidl (AT402743)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Oepping 1'), 'Klaus Leitenbauer (AT402722)');
+
 insert into team (name) values ('ATSV Waldneukirchen Stocksport 1');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ATSV Waldneukirchen Stocksport 1'), 'Johann Wallner (AT401286)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ATSV Waldneukirchen Stocksport 1'), 'Robert Oberngruber (AT401276)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ATSV Waldneukirchen Stocksport 1'), 'Gerhard Zerfas (AT401288)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ATSV Waldneukirchen Stocksport 1'), 'Karl Aspetsberger (AT401262)');
+
 insert into team (name) values ('ÖTSU Arnreit 1');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Arnreit 1'), 'Peter Sommer (AT402529)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Arnreit 1'), 'Kurt Neundlinger (AT402514)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Arnreit 1'), 'Norbert Engleder (AT402497)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Arnreit 1'), 'Manfred Gierlinger (AT402501)');
+
 insert into team (name) values ('Union Stocksport Putzleinsdorf 1');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'Union Stocksport Putzleinsdorf 1'), 'Robert Höglinger (AT405693)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'Union Stocksport Putzleinsdorf 1'), 'Gottfried Peer (AT405707)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'Union Stocksport Putzleinsdorf 1'), 'Bernhard Falkinger (AT405688)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'Union Stocksport Putzleinsdorf 1'), 'Josef Prokesch (AT405711)');
+
 insert into team (name) values ('ESV Ruprechtshofen 1');
-insert into team (name) values ('ÖTSU Strolheim 1');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ESV Ruprechtshofen 1'), 'Helmut Stadlbauer (AT401998)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ESV Ruprechtshofen 1'), 'Karl Lettner (AT401986)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ESV Ruprechtshofen 1'), 'Josef Lettner (AT401987)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ESV Ruprechtshofen 1'), 'Johann Hintersteininger (AT401981)');
+
+insert into team (name) values ('ÖTSU Stroheim 1');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Stroheim 1'), 'Günter Reisinger (AT402887)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Stroheim 1'), 'Friedrich Mairhuber (AT402884)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Stroheim 1'), 'Emil Hauzenberger (AT402879)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Stroheim 1'), 'Johann Reisinger (AT402888)');
+
 insert into team (name) values ('Union ESV Haag a. H. 1');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'Union ESV Haag a. H. 1'), 'Andreas Gervasi (AT405453)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'Union ESV Haag a. H. 1'), 'Rupert Traxler (AT405468)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'Union ESV Haag a. H. 1'), 'Walter Deixler (AT405448)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'Union ESV Haag a. H. 1'), 'Hubert Rebhan (AT405464)');
+
 insert into team (name) values ('ÖTSU Oepping 2');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Oepping 2'), 'Peter Magauer (AT402725)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Oepping 2'), 'Reinhold Stallinger (AT402742)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Oepping 2'), 'Franz Magauer (AT402726)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Oepping 2'), 'Manfred Obermüller (AT402731)');
+
 insert into team (name) values ('SU Schardenberg 2');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Schardenberg 2'), 'Johann Duscher (AT404363)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Schardenberg 2'), 'Josef Bauer (AT404359)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Schardenberg 2'), 'Gerhard Osterkorn (AT404382)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Schardenberg 2'), 'Markus Haas (AT404370)');
 
-insert into player (team_teamid, name) values(1, 'Christian Spindler');
-insert into player (team_teamid, name) values(1, 'Johann Märzinger');
-insert into player (team_teamid, name) values(1, '');
-insert into player (team_teamid, name) values(1, '');
+insert into team (name) values ('SU Niederwaldkirchen 1');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Niederwaldkirchen 1'), 'Helmut Lindorfer (AT404163)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Niederwaldkirchen 1'), 'Alfred Hauder (AT404152)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Niederwaldkirchen 1'), 'Erich Hametner (AT404151)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Niederwaldkirchen 1'), 'Thomas Lauß (AT404158)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Niederwaldkirchen 1'), 'August Staltner (AT404185)');
 
-insert into player (team_teamid, name) values(2, 'Johann Wallner');
-insert into player (team_teamid, name) values(2, 'Robert Obengruber');
-insert into player (team_teamid, name) values(2, '');
-insert into player (team_teamid, name) values(2, '');
+insert into team (name) values ('SU Schweinbach 1');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Schweinbach 1'), 'Gottfried Walter (AT404451)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Schweinbach 1'), 'Johann Gaffl (AT404436)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Schweinbach 1'), 'Harald Lang (AT404444)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Schweinbach 1'), 'Johannes Minichberger (AT404445)');
 
-insert into player (team_teamid, name) values(3, 'Peter Sommer');
-insert into player (team_teamid, name) values(3, 'Kurt Neundinger');
-insert into player (team_teamid, name) values(3, '');
-insert into player (team_teamid, name) values(3, '');
+insert into team (name) values ('ESV Traun 2');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ESV Traun 2'), 'Manfred Schober (AT402153)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ESV Traun 2'), 'Erwin Iraneck (AT402149)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ESV Traun 2'), 'Christian Fragner (AT402144)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ESV Traun 2'), 'Johann Holzinger (AT402148)');
 
-insert into player (team_teamid, name) values(4, 'Robert Höglinger');
-insert into player (team_teamid, name) values(4, 'Gottfried Peer');
-insert into player (team_teamid, name) values(4, '');
-insert into player (team_teamid, name) values(4, '');
+insert into team (name) values ('SU Schardenberg 1');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Schardenberg 1'), 'Johann Gradinger (AT404369)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Schardenberg 1'), 'Karl Flick (AT404366)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Schardenberg 1'), 'Helmut Haas (AT404371)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'SU Schardenberg 1'), 'Helmut Eggertsberger (AT404365)');
 
-insert into player (team_teamid, name) values(5, 'Helmut Stadlbauer');
-insert into player (team_teamid, name) values(5, 'Karl Lettner');
-insert into player (team_teamid, name) values(5, '');
-insert into player (team_teamid, name) values(5, '');
+insert into team (name) values ('ESV Bad Goisern 1');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ESV Bad Goisern 1'), 'Heinz Koch (AT401616)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ESV Bad Goisern 1'), 'Willibald Huber (AT405990)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ESV Bad Goisern 1'), 'Horst Höll (AT401612)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ESV Bad Goisern 1'), 'Ernst Kefer (AT401614)');
 
-insert into player (team_teamid, name) values(6, 'Günter Reisinger');
-insert into player (team_teamid, name) values(6, 'Friedrich Mairhuber');
-insert into player (team_teamid, name) values(6, '');
-insert into player (team_teamid, name) values(6, '');
+insert into team (name) values ('TSU Wartberg/Aist 1');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'TSU Wartberg/Aist 1'), 'Hubert Stegfellner (AT405265)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'TSU Wartberg/Aist 1'), 'Julius Ehrensperger (AT405237)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'TSU Wartberg/Aist 1'), 'Josef Stegfellner (AT405263)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'TSU Wartberg/Aist 1'), 'Wilhelm Schreiner (AT405262)');
 
-insert into player (team_teamid, name) values(7, 'Andreas Gervasi');
-insert into player (team_teamid, name) values(7, 'Rupert Taxler');
-insert into player (team_teamid, name) values(7, '');
-insert into player (team_teamid, name) values(7, '');
+insert into team (name) values ('ÖTSU Oepping 3');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Oepping 3'), 'Walter Kern (AT402713)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Oepping 3'), 'Franz Kern (AT402714)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Oepping 3'), 'Alois Wögerbauer (AT402748)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ÖTSU Oepping 3'), 'Walter Spindlbalker (AT402738)');
 
-insert into player (team_teamid, name) values(8, 'Peter Magauer');
-insert into player (team_teamid, name) values(8, 'Reinhold Stallinger');
-insert into player (team_teamid, name) values(8, '');
-insert into player (team_teamid, name) values(8, '');
+insert into team (name) values ('ASKÖ Schwertberg 1');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ASKÖ Schwertberg 1'), 'Franz Wurm (AT400786)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ASKÖ Schwertberg 1'), 'Rudolf Bauer (AT400773)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ASKÖ Schwertberg 1'), 'Johann Kastner (AT400780)');
+insert into player (team_teamid, name) values ((select teamid from team where name = 'ASKÖ Schwertberg 1'), 'Gerhard Reiter (AT400785)');
 
-insert into player (team_teamid, name) values(9, 'Johann Duscher');
-insert into player (team_teamid, name) values(9, 'Josef Bauer');
-insert into player (team_teamid, name) values(9, '');
-insert into player (team_teamid, name) values(9, '');
+insert into team (name) values ('1. ESV Haslach an der Mühl 1');
+insert into player (team_teamid, name) values ((select teamid from team where name = '1. ESV Haslach an der Mühl 1'), 'Christian Jell (AT400013)');
+insert into player (team_teamid, name) values ((select teamid from team where name = '1. ESV Haslach an der Mühl 1'), 'Peter Schauer (AT400020)');
+insert into player (team_teamid, name) values ((select teamid from team where name = '1. ESV Haslach an der Mühl 1'), 'Wilfried Walchshofer (AT400026)');
+insert into player (team_teamid, name) values ((select teamid from team where name = '1. ESV Haslach an der Mühl 1'), 'Robert Spindelböck (AT400021)');
