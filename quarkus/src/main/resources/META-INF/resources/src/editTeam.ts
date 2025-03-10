08fa90d2e4dfa559fc6385f989d403a464df82cc
Atilla Simsek <a.simsek@students.htl-leonding.ac.at>
2025-03-10 19:51:58 +0100

    pushed not finished code from editTeam

--- quarkus/src/main/resources/META-INF/resources/src/editTeam.ts
+++ quarkus/src/main/resources/META-INF/resources/src/editTeam.ts
@@ -23,4 +23,43 @@ function fillInputFields() {
 
 }
 
+
+async function updateTeam() {
+    try {
+        const teamId = JSON.parse(sessionStorage.getItem('teamToEdit')).teamId;
+        const teamName = (document.getElementById('teamname') as HTMLInputElement).value;
+        const response = await fetch(`http://localhost:8080/api/team/updateTeam?teamId=${teamId}&teamName=${teamName}`, {
+            method: "PUT"
+        });
+        console.log(await response.text());
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+async function updatePlayer(playerId: number) {
+    try {
+        const playerName = (document.getElementById(`player${playerId}`) as HTMLInputElement).value;
+        const teamData = sessionStorage.getItem("teamToEdit");
+
+        if (!teamData) {
+            console.error("No team data found in session storage");
+            return;
+        }
+
+        const teamId = JSON.parse(teamData).teamId;
+
+        const url = `http://localhost:8080/api/players/updatePlayer?playerId=${playerId}&teamId=${teamId}&playerName=${encodeURIComponent(playerName)}`;
+
+        const response = await fetch(url, {
+            method: "PUT"
+        });
+
+        console.log(await response.text());
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', fillInputFields);
