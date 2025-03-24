757ab45160c6aaa83dd90d7ff7fb232fde71a951
marvin-anderson <m.anderson@students.htl-leonding.ac.at>
2025-03-25 00:41:16 +0100

    Feat: added maven Wrapper and Buildscript for future cicd

---
+++ build.sh
@@ -0,0 +1,16 @@
+# DEV ONLY, NOT NEEDED WHEN DEPLOYING
+export MAVEN_OPTS="-Dnet.bytebuddy.experimental=true"
+
+# Build frontend, move to resources
+rm -rf backend/src/main/resources/META-INF/resources/*
+cd frontend
+tsc
+mv ./dist ../backend/src/main/resources/META-INF/resources/dist
+cp -r ./pages ../backend/src/main/resources/META-INF/resources
+
+# create jar
+cd ../backend
+./mvnw clean package || { echo "Maven build failed"; exit 1; }
+
+cd ../docker
+docker compose down -v && docker compose up --build
