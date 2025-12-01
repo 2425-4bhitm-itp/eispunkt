867f3b29c5f21298ae7a97f9bbbd55f6585928b9
FlorianHayer <f.hayer@students.htl-leonding.ac.at>
2025-12-01 13:53:08 +0100

    fix: typo int tournament repository, path renamed to api/teams

--- backend/src/main/java/at/ac/htlleonding/routes/TeamResource.java
+++ backend/src/main/java/at/ac/htlleonding/routes/TeamResource.java
@@ -9,7 +9,7 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriBuilder;
 
-@Path("/api/team")
+@Path("/api/teams")
 public class TeamResource {
     @Inject
     TeamRepository teamRepository;
